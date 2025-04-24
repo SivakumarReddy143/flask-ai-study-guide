e1798653cdf193de0d0518e05f562c30edeab517
SivakumarReddy143 <mshivakumarreddy78@gmail.com>
2025-04-24 10:00:17 +0530

    code updated

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     
     # Disable auto-reloader but keep debug mode
     app.run(
-        host="127.0.0.1", 
+        host="0.0.0.0", 
         port=5000, 
         debug=True,
         use_reloader=False  # Disable auto-reloader
